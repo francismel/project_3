cb965c621379a28dbc8c09951c210a2497b10b89
Francis Mel <fmeldefo@usc.edu>
2020-03-17 16:18:01 -0700

    up to date

--- ga_forum/blog/models.py
+++ ga_forum/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    strContent = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.author} said: {self.strContent} on {self.date}'
--- ga_forum/blog/views.py
+++ ga_forum/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
 
 
 def home(request):
-    return render(request,'home.html')
+
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request,'home.html',context)
