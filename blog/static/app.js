454faaba302036e5b7af7c56d851af0340111ea9
Hunner <h.herrera.academics@gmail.com>
2020-03-23 09:41:59 -0700

    add comment

--- blog/static/app.js
+++ blog/static/app.js
@@ -19,17 +19,3 @@ function showProfile(element) {
   }
 
 
-
- function showComment(e) {
-        console.log(e)
-        $(e).append( $('.myDIV').attr("style","display: show") )
-
-        // var x = document.getElementById("myDIV");
-        // if (x.style.display === "none") {
-        //   x.style.display = "block";
-        // } else {
-        //   x.style.display = "none";
-        // }
-      }
-
-
