1f9e429889cdca24593f1ac07fe00d11ca2995ab
Hunner <h.herrera.academics@gmail.com>
2020-03-23 16:19:57 -0700

    add post functionality

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .models import Reply
-from .forms import PostForm
+# from .forms import PostForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 
@@ -15,30 +15,29 @@ def home(request):
 
 
 def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)  
-        if form.is_valid(): 
-            print('hit if')
-            form.save()  
-            return redirect('blog-home')
-        else:
-            print('hit else')
-            return redirect('blog-home')
-            
+    post = Post( author = request.user, strContent = request.POST.get('post-input') )
+    print(post)
+    post.save()
+    next = request.POST.get('currentpath', '/')
+    return redirect(next)
+
+
+    # if request.method == 'POST':
+    #     print('rquest post: ', request.POST)
+    #     form = PostForm(request.POST)  
+    #     if form.is_valid(): 
+    #         print('hit if')
+    #         form.save()  
+    #         return redirect('blog-home')
+    #     else:
+    #         print('hit else')
+    #         return redirect('blog-home')
     # post = Post.objects.create()
     # return render(request, "blog/home.html", context)
 
 def reply_create(request, post_id):
     post = Post.objects.get(id = post_id)
-    print(post)
-    print('post id: ', post_id)
-
-    print('User Id: ',request.user.id)
-    print('Reply Content Id: ',request.POST.get('input-comment'))
-    
     reply = Reply( author = request.user, post = post, strContent = request.POST.get('input-comment') )
     reply.save()
-    print(reply)
-
-    next = request.POST.get('next', '/')
+    next = request.POST.get('currentpath', '/')
     return redirect(next)
